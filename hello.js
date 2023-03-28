5cd184dd64bf10ac503c972f8dc03ab5e46fc3f4
akash kumar verma <akkiverma04@gmail.com>
2023-03-29 03:03:13 +0530

    init commit

--- hello.js
+++ hello.js
@@ -1,2 +1,4 @@
 /* git remote add remotename git@github.com:oakhen/weather-app.git dalne se  */
-/* remotename mere andar save ho gaya  */
+/* remotename mere andar save ho gaya remotename ko hum push kar sakte hai */
+
+/* git push -u remotename master */
